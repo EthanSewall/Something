5b194cfdc5f6512a19c973e869c0a15049839398
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-08-13 11:33:45 -0700

    Variables and such

---
+++ Variables and such/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_and_such
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's a good day to teleport!");
+            Console.ReadKey();
+        }
+    }
+}
