6c4aaf0fd2de6353d066cab96cdfe2af0a251135
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-08-13 17:40:24 -0700

    the counting thing

---
+++ counting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stillGoing = true;
+            short number = 0;
+               
+           while (stillGoing)
+            {
+                number++;
+                Console.WriteLine(number.ToString());  
+            }
+
+        }
+    }
+}
