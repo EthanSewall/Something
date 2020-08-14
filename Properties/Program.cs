85465f889a7e891da9ca1fd78ae25d1afb1841c4
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-08-14 12:46:01 -0700

    properties exercise- exchanger

---
+++ Properties/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Properties
+{
+    class Program
+    {
+        float money = 2;
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+        void WriteTheThings()
+        {
+            Console.WriteLine(USD + " USD = " + canadian + "canadian");
+        }
+
+
+        public float USD
+        {
+            get
+            {
+                return money;
+            }
+        }
+
+        public float canadian
+        {
+            get 
+            {
+                return money * (1.33f);
+            }
+        }
+    }
+}
