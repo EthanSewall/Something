c57ba41b2167d89d27ca7bbe5a477674d5541dd3
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-08-14 10:41:42 -0700

    Methods exercise

---
+++ methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Game currentGame = new Game();
+           currentGame.score = 100;
+            currentGame.Start();
+
+        }
+    }
+
+    class Game
+    {
+        public int score = 0;
+
+        public void Start()
+        {
+            Console.WriteLine(score);
+            PrintScore(12);
+            PrintScore(18);
+            PrintScore(20);
+
+            Console.ReadKey();
+        }
+
+        public void PrintScore(int add)
+        {
+            Console.WriteLine("Score increase: " + AddToScore(add));
+        }
+
+        public int AddToScore(int add)
+        {
+            score += add;
+            return score;
+        }
+
+    }
+}
