522aeca0d7c74405288f3a98f2ceae5ed2a32b65
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-08-19 17:13:34 -0700

    scope exercise

---
+++ Bank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Bank
+    {
+        private float currentBalance;
+        public void Deposit(float net)
+        {
+            currentBalance += net;
+        }
+        public float Withdraw()
+        {
+            float retrievedBalance = currentBalance;
+            currentBalance = 0;
+            return retrievedBalance;
+        }
+        public float Balance()
+        {
+            return currentBalance;
+        }
+    }
+}
---
+++ TV/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Television
+    {
+        private int currentChannel = 7; 
+        private int currentVolume = 15;
+        
+        public void IncreaseVolume()
+        {
+            currentVolume++;
+        }   
+        public void DecreaseVolume()
+        {
+            currentVolume--;
+        }   
+        public int Volume()
+        {
+            return currentVolume;
+        } 
+        public void IncreaseChannel()
+        {
+            currentChannel++;
+        }
+        public void DecreaseChannel()
+        {
+            currentChannel--;
+        }
+        public int Channel()     
+        {
+            return currentChannel;
+        }
+    
+    }
+}
