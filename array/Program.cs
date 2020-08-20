19d9bde275928118670145e4d349f6d2684e4f1a
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-08-20 11:52:32 -0700

    array exercise

---
+++ array/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static Team theTeam = new Team();
+        static void Main(string[] args)
+        {
+            theTeam.PrintAverage();
+        }
+    }
+
+    class Employee
+    {
+        public string name1;
+        public string name2;
+        public int experience;
+    }
+
+    class Team
+    {
+        Employee[] group = new Employee[4];
+        Employee leader = new Employee();
+
+        public void PrintAverage()
+        {
+            float average = 0;
+            int divisor = 0;
+            foreach(Employee e in group)
+            {
+                average += e.experience;
+                divisor++;
+            }
+            average += leader.experience;
+            divisor++;
+            average /= divisor;
+
+            Console.WriteLine("The average experience is " +average.ToString() + "years.");
+        }
+    }
+}
