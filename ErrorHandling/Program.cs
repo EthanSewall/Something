b2e00a4b38ed71f343987098b146f1135af01366
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-08-27 10:51:55 -0700

    errors and conversion

---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = 1;
+            try
+            {
+                int e = h / 0;
+            }
+            catch (System.DivideByZeroException except)
+            {
+                Console.WriteLine(except);
+            }
+            finally
+            {
+               
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ typeConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace typeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = 1;
+            long e = long.MaxValue;
+
+            Console.WriteLine(e);
+            h = (int)e;
+            Console.WriteLine(h);
+
+
+            Console.ReadKey();
+        }
+    }
+}
